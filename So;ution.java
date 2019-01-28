d83a0f11cc87a3c4cd629b0ecc87e434689fc766
sdim-ua <ingersol@yandex.ua>
2019-01-28 21:53:38 +0400

    Create So;ution.java

---
+++ So;ution.java
@@ -0,0 +1,15 @@
+/* 
+Человечки
+*/
+
+public class Person {
+    //напишите тут ваш код
+    String name;
+    int age;
+    String address;
+    char sex;
+    
+    public static void main(String[] args) {
+
+    }
+}
